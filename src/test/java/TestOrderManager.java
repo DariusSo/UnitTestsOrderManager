8c78c92e7bf1f8b52041dd1681e73153c5562517
Darius <darius.songaila1@gmail.com>
2024-06-03 18:07:28 +0300

    Done

--- src/test/java/TestOrderManager.java
+++ src/test/java/TestOrderManager.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.function.Executable;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestOrderManager {
     OrderManagerImpl impl;
 
@@ -19,7 +21,7 @@ public class TestOrderManager {
         impl.addOrder(order);
         Order order1 = impl.getOrderById(1);
         //Assert
-        Assertions.assertEquals(order, order1);
+        assertEquals(order, order1);
     }
     @Test
     public void checkIfAddOrderAddsDoesntAcceptDublicateId(){
@@ -30,7 +32,7 @@ public class TestOrderManager {
         impl.addOrder(order);
         impl.addOrder(order1);
         //Assert
-        Assertions.assertEquals(1, 1);
+        assertEquals(1, 1);
     }
     @Test
     public void checkIfAddOrderThrowsExceptionWhenOrderIsNull(){
@@ -42,7 +44,7 @@ public class TestOrderManager {
         impl.addOrder(order);
         impl.addOrder(order1);
         //Assert
-        Assertions.assertThrows(NullPointerException.class, ()->{impl.addOrder(order2);});
+        assertThrows(NullPointerException.class, ()->{impl.addOrder(order2);});
     }
 
     @Test
@@ -53,7 +55,7 @@ public class TestOrderManager {
         impl.removeOrder(1);
         Order order1 = impl.getOrderById(1);
         //Assert
-        Assertions.assertNotEquals(order, order1);
+        assertNotEquals(order, order1);
     }
     @Test
     public void removeOrderDoesntRemoveOrderFromList() {
@@ -64,7 +66,7 @@ public class TestOrderManager {
         impl.removeOrder(2);
         Order order1 = impl.getOrderById(1);
         //Assert
-        Assertions.assertEquals(order, order1);
+        assertEquals(order, order1);
     }
     @Test
     public void getOrderByIdReturnsCorrectOrder(){
@@ -76,7 +78,7 @@ public class TestOrderManager {
         //Act
         Order oreder1 = impl.getOrderById(2);
         //Assert
-        Assertions.assertEquals(order2, oreder1);
+        assertEquals(order2, oreder1);
     }
     @Test
     public void getOrderByIdDoesntFindOrderReturnsNull(){
@@ -86,7 +88,7 @@ public class TestOrderManager {
         //Act
         Order oreder1 = impl.getOrderById(2);
         //Assert
-        Assertions.assertNull(oreder1);
+        assertNull(oreder1);
     }
     @Test
     public void getOrderByCustomerReturnsCorrectOrder(){
@@ -100,8 +102,8 @@ public class TestOrderManager {
         //Act
         List<Order> clientOrderList = impl.getOrdersByCustomer("2");
         //Assert
-        Assertions.assertTrue(clientOrderList.contains(order3));
-        Assertions.assertTrue(clientOrderList.contains(order2));
+        assertTrue(clientOrderList.contains(order3));
+        assertTrue(clientOrderList.contains(order2));
     }
     @Test
     public void getOrderByCustomerReturnsFalseWhenCantFindCustomer(){
@@ -112,7 +114,7 @@ public class TestOrderManager {
         //Act
         List<Order> clientOrderList = impl.getOrdersByCustomer("2");
         //Assert
-        Assertions.assertFalse(clientOrderList.contains(order));
+        assertFalse(clientOrderList.contains(order));
     }
     @Test
     public void calculateTotalRevenueReturnsCorrectAmountSum(){
@@ -126,7 +128,7 @@ public class TestOrderManager {
         //Act
         double test = impl.calculateTotalRevenue();
         //Assert
-        Assertions.assertEquals(73.20, test);
+        assertEquals(73.20, test);
     }
     @Test
     public void calculateTotalRevenueReturnsCorrectAmountSumForSameClient(){
@@ -140,7 +142,7 @@ public class TestOrderManager {
         //Act
         double test = impl.calculateTotalRevenue();
         //Assert
-        Assertions.assertEquals(73.20, test);
+        assertEquals(73.20, test);
     }
     @Test
     public void updateOrderStatusChangesOrderStatusAndReturnsTrue(){
@@ -152,8 +154,8 @@ public class TestOrderManager {
         boolean test = impl.updateOrderStatus(10, "Done");
         List<Order> checkOrder = impl.filterOrdersByStatus("Done");
         //Assert
-        Assertions.assertTrue(test);
-        Assertions.assertTrue(checkOrder.contains(order));
+        assertTrue(test);
+        assertTrue(checkOrder.contains(order));
     }
     @Test
     public void updateOrderStatusChangesOrderStatusAndReturnsFalseWhenCantFindOrder(){
@@ -163,7 +165,7 @@ public class TestOrderManager {
         //Act
         boolean test = impl.updateOrderStatus(11, "Done");
         //Assert
-        Assertions.assertFalse(test);
+        assertFalse(test);
     }
     @Test
     public void filterOrderByStatusReturnsDoneOrderList(){
@@ -177,9 +179,9 @@ public class TestOrderManager {
         //Act
         List<Order> checkList = impl.filterOrdersByStatus("Done");
         //Assert
-        Assertions.assertEquals(2, checkList.size());
-        Assertions.assertTrue(checkList.contains(order));
-        Assertions.assertTrue(checkList.contains(order3));
+        assertEquals(2, checkList.size());
+        assertTrue(checkList.contains(order));
+        assertTrue(checkList.contains(order3));
     }
     @Test
     public void filterOrderByStatusReturnsEmptyListWhenNoOrdersFound(){
@@ -189,7 +191,7 @@ public class TestOrderManager {
         //Act
         List<Order> checkList = impl.filterOrdersByStatus("Done");
         //Assert
-        Assertions.assertEquals(0, checkList.size());
+        assertEquals(0, checkList.size());
     }
     @Test
     public void getOrdersAboveCertainValueGetsOrdersAbove10(){
@@ -203,8 +205,8 @@ public class TestOrderManager {
         //Act
         List<Order> checkList = impl.getOrdersAboveCertainValue(10.00);
         //Assert
-        Assertions.assertTrue(checkList.contains(order2));
-        Assertions.assertTrue(checkList.contains(order3));
+        assertTrue(checkList.contains(order2));
+        assertTrue(checkList.contains(order3));
     }
     @Test
     public void getOrdersAboveCertainValueReturnsEmptyListWhenThereAreNoValuesAbove10(){
@@ -218,7 +220,7 @@ public class TestOrderManager {
         //Act
         List<Order> checkList = impl.getOrdersAboveCertainValue(10.00);
         //Assert
-        Assertions.assertEquals(0, checkList.size());
+        assertEquals(0, checkList.size());
     }
 
     @BeforeEach
