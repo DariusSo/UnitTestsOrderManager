35e5409963d170c4397724984cc999e670d7d6b8
Darius <darius.songaila1@gmail.com>
2024-06-03 17:58:11 +0300

    Done

--- src/test/java/TestOrderManager.java
+++ src/test/java/TestOrderManager.java
@@ -91,7 +91,6 @@ public class TestOrderManager {
     @Test
     public void getOrderByCustomerReturnsCorrectOrder(){
         //Arrange
-        List<Order> tempList = new ArrayList<>();
         Order order = new Order(1, "1", 23.56, "preparing...");
         Order order2 = new Order(2, "2", 23.56, "preparing...");
         Order order3 = new Order(3, "2", 23.56, "preparing...");
@@ -116,6 +115,111 @@ public class TestOrderManager {
         Assertions.assertFalse(clientOrderList.contains(order));
     }
     @Test
+    public void calculateTotalRevenueReturnsCorrectAmountSum(){
+        //Arrange
+        Order order = new Order(10, "12",24.50, "Preparing...");
+        Order order1 = new Order(11, "13",24.10, "Preparing...");
+        Order order2 = new Order(12, "14",24.60, "Preparing...");
+        impl.addOrder(order);
+        impl.addOrder(order1);
+        impl.addOrder(order2);
+        //Act
+        double test = impl.calculateTotalRevenue();
+        //Assert
+        Assertions.assertEquals(73.20, test);
+    }
+    @Test
+    public void calculateTotalRevenueReturnsCorrectAmountSumForSameClient(){
+        //Arrange
+        Order order = new Order(10, "12",24.50, "Preparing...");
+        Order order1 = new Order(11, "12",24.10, "Preparing...");
+        Order order2 = new Order(12, "12",24.60, "Preparing...");
+        impl.addOrder(order);
+        impl.addOrder(order1);
+        impl.addOrder(order2);
+        //Act
+        double test = impl.calculateTotalRevenue();
+        //Assert
+        Assertions.assertEquals(73.20, test);
+    }
+    @Test
+    public void updateOrderStatusChangesOrderStatusAndReturnsTrue(){
+        //Arrange
+        Order order = new Order(10, "12",24.50, "Preparing...");
+        Order order1 = new Order(11, "13",24.50, "Preparing...");
+        impl.addOrder(order);
+        //Act
+        boolean test = impl.updateOrderStatus(10, "Done");
+        List<Order> checkOrder = impl.filterOrdersByStatus("Done");
+        //Assert
+        Assertions.assertTrue(test);
+        Assertions.assertTrue(checkOrder.contains(order));
+    }
+    @Test
+    public void updateOrderStatusChangesOrderStatusAndReturnsFalseWhenCantFindOrder(){
+        //Arrange
+        Order order = new Order(10, "12",24.50, "Preparing...");
+        impl.addOrder(order);
+        //Act
+        boolean test = impl.updateOrderStatus(11, "Done");
+        //Assert
+        Assertions.assertFalse(test);
+    }
+    @Test
+    public void filterOrderByStatusReturnsDoneOrderList(){
+        //Arrange
+        Order order = new Order(1, "1", 23.56, "Done");
+        Order order2 = new Order(2, "2", 23.56, "preparing...");
+        Order order3 = new Order(3, "2", 23.56, "Done");
+        impl.addOrder(order);
+        impl.addOrder(order2);
+        impl.addOrder(order3);
+        //Act
+        List<Order> checkList = impl.filterOrdersByStatus("Done");
+        //Assert
+        Assertions.assertEquals(2, checkList.size());
+        Assertions.assertTrue(checkList.contains(order));
+        Assertions.assertTrue(checkList.contains(order3));
+    }
+    @Test
+    public void filterOrderByStatusReturnsEmptyListWhenNoOrdersFound(){
+        //Arrange
+        Order order2 = new Order(2, "2", 23.56, "preparing...");
+        impl.addOrder(order2);
+        //Act
+        List<Order> checkList = impl.filterOrdersByStatus("Done");
+        //Assert
+        Assertions.assertEquals(0, checkList.size());
+    }
+    @Test
+    public void getOrdersAboveCertainValueGetsOrdersAbove10(){
+        //Arrange
+        Order order = new Order(1, "1", 9.99, "Done");
+        Order order2 = new Order(2, "2", 23.56, "preparing...");
+        Order order3 = new Order(3, "2", 10.01, "Done");
+        impl.addOrder(order);
+        impl.addOrder(order2);
+        impl.addOrder(order3);
+        //Act
+        List<Order> checkList = impl.getOrdersAboveCertainValue(10.00);
+        //Assert
+        Assertions.assertTrue(checkList.contains(order2));
+        Assertions.assertTrue(checkList.contains(order3));
+    }
+    @Test
+    public void getOrdersAboveCertainValueReturnsEmptyListWhenThereAreNoValuesAbove10(){
+        //Arrange
+        Order order = new Order(1, "1", 9.99, "Done");
+        Order order2 = new Order(2, "2", 8.52, "preparing...");
+        Order order3 = new Order(3, "2", 7.49, "Done");
+        impl.addOrder(order);
+        impl.addOrder(order2);
+        impl.addOrder(order3);
+        //Act
+        List<Order> checkList = impl.getOrdersAboveCertainValue(10.00);
+        //Assert
+        Assertions.assertEquals(0, checkList.size());
+    }
 
     @BeforeEach
     public void beforeEach(){
